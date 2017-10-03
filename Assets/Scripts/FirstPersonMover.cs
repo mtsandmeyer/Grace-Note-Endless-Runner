c80b245f9a45908145d5be95d3d990114e2e6b11
mtsandmeyer <32112441+mtsandmeyer@users.noreply.github.com>
2017-10-02 23:05:27 -0400

    First commit

---
+++ Assets/Scripts/FirstPersonMover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FirstPersonMover : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    public void Start ()
+    {
+        
+    }
+    
+    public void Update ()
+    {
+        Vector3 delta = new Vector3( Input.GetAxis( "Horizontal" ), 0.0f,  Input.GetAxis( "Vertical" ) );
+
+        if ( GetComponent<NavMeshAgent>().enabled )
+        {
+            if ( delta != Vector3.zero )
+            {
+                //transform.position += delta * Time.deltaTime * speed;
+                GetComponent<NavMeshAgent>().destination = transform.position + delta;
+            }
+            else 
+            {
+                GetComponent<NavMeshAgent>().destination = transform.position;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MovementAnimatorUpdater.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(NavMeshAgent))]
+public class MovementAnimatorUpdater : MonoBehaviour
+{
+    public float walkingSpeedThreshold = 0.3f;
+    protected Animator animator;
+    protected NavMeshAgent agent;
+    protected Rigidbody body;
+
+    public void Awake()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        body = GetComponent<Rigidbody>();
+    }
+    
+    public void Update()
+    {
+        animator.SetBool( "jumping", agent.isOnOffMeshLink || !body.isKinematic );
+        animator.SetFloat( "speed", agent.velocity.sqrMagnitude / ( agent.speed * agent.speed ) );
+    }
+}
